e2801f7f272700bbe4d6e2b183fa56741b7130f5
BenIDS <ben@i-disposals.com>
2026-03-11 12:30:19 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -353,7 +353,8 @@ export default function App() {
     setScanLog([]);
     try {
       const res = await fetch(`${API_BASE}/scrape`, {
-        headers: { "Authorization": `Bearer ${token}` }
+        method: "POST",
+        headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` }
       });
       const data = await res.json();
       if (data.log) data.log.forEach(l => setScanLog(p => [...p, l]));
